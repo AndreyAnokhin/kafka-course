076314d578911b3aead2952db09198b4145a29fe
Andrei Anokhin <andrei_anokhin@epam.com>
2021-08-22 21:14:15 +0300

    Initial commit

---
+++ src/main/java/com/github/andreyanokhin/kafka/tutorial1/ProducerDemo.java
@@ -0,0 +1,10 @@
+package com.github.andreyanokhin.kafka.tutorial1;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+    }
+}
